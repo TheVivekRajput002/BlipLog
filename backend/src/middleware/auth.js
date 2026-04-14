a1314e5b7b2fe0567a16bd94b23274abace3f5f9
Vivek Rajput <work.tvivekrajput@gmail.com>
2026-04-14 18:33:38 +0530

    temp auth.route.js added

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,10 @@
+// temp
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ ok: true, message: "auth route working" });
+});
+
+module.exports = router;
